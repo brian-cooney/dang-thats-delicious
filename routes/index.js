b97647ba0f2012eab8863dd3fd14ff1e410dfd0d
brian-cooney <bcooney@gmail.com>
2017-11-22 00:02:42 +0000

    gitignore keys

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const brian = {name: 'Brian', age: 36, trendy: true };
+  // res.send('Hey! It works!');
+  // res.json(brian);
+  res.render('Hello');
+});
+
+router.get('/reverse/:name', (req,res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
