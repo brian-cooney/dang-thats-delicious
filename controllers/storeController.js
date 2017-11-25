594a32b84ee7039595109ff5803ba6915d894316
brian-cooney <bcooney@gmail.com>
2017-11-25 18:11:38 +0000

    Add store route

---
+++ controllers/storeController.js
@@ -0,0 +1,23 @@
+import { request } from 'http';
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+exports.homePage = (req, res) => {
+    console.log(req.name);
+    req.flash('error', 'S')
+    res.render('index');
+};
+
+exports.addStore = (req, res) => {
+    res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore = async (req, res) => {
+    const store = (new Store(req.body)).save();
+    req.flash('success', `Successfully Created ${store.name}. care to leave a review?`);
+    res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = (req, res) => {
+    res.render('stores', { title: 'Stores' });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
+const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
-router.get('/', (req, res) => {
-  const brian = {name: 'Brian', age: 36, trendy: true };
-  // res.send('Hey! It works!');
-  // res.json(brian);
-  res.render('Hello');
-});
-
-router.get('/reverse/:name', (req,res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+router.get('/', storeController.getStores );
+router.get('/stores', storeController.getStores );
+router.get('/add', storeController.addStore );
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
